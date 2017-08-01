e52cca48013174a93d184066f70a540102eced72
Jan-Hendrik Grundhöfer <jan-hendrik.grundhoefer@web.de>
2017-08-01 17:58:59 +0200

    Initial setup. (#1)
    * Initial setup.
    
    * Installing missing packages.
    
    * Add get-certificate module. Create documentation. Some more tests (currently not working).
    
    * Fixed unit tests.
    
    * Remove logging. Added callback result.
    
    * Some more improvements.
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # https-or-http
 https-or-http runs an HTTPS server or, if not possible, an HTTP server.
+
+## Quick start
+
+First you need to add a reference to https-or-http to your application:
+
+```javascript
+const httpsOrHttp = require('https-or-http');
+```
+
+Then use the `httpsOrHttp` function to run an HTTPS server. You need to provide an Express application, a directory that contains a certificate and a private key, and the ports to be used. If the HTTPS could not be started, e.g. because there is no certificate or no private key, an HTTP server is started as fallback.
+
+*Please note that the directory must contain a `certificate.pem` and a `privateKey.pem` file, otherwise an HTTP server will be started.*
+
+As a result you get an object that describes the servers that have been started:
+
+```javascript
+const app = express();
+
+httpsOrHttp({
+  app,
+  certificateDirectory: '/...',
+  ports: {
+    https: 8000,
+    http: 9000
+  }
+}, (err, servers) => {
+  // ...
+});
+```
+
+## Running the build
+
+To build this module use [roboter](https://www.npmjs.com/package/roboter).
+
+```bash
+$ bot
+```
+
+## License
+
+The MIT License (MIT)
+Copyright (c) 2017 the native web. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ test/units/httpsOrHttpTests.js
@@ -0,0 +1,112 @@
+'use strict';
+
+const path = require('path');
+
+const assert = require('assertthat'),
+      express = require('express'),
+      freeport = require('freeport');
+
+const httpsOrHttp = require('../../lib/httpsOrHttp');
+
+suite('httpsOrHttp', () => {
+  let portHttp,
+      portHttps;
+
+  setup(done => {
+    freeport((errFirst, portFirst) => {
+      if (errFirst) {
+        throw errFirst;
+      }
+
+      portHttp = portFirst;
+
+      freeport((errSecond, portSecond) => {
+        if (errSecond) {
+          throw errSecond;
+        }
+
+        portHttps = portSecond;
+
+        done();
+      });
+    });
+  });
+
+  test('is a function.', done => {
+    assert.that(httpsOrHttp).is.ofType('function');
+    done();
+  });
+
+  test('throws an error if options are missing.', done => {
+    assert.that(() => {
+      httpsOrHttp();
+    }).is.throwing('Options are missing.');
+    done();
+  });
+
+  test('throws an error if app is missing.', done => {
+    assert.that(() => {
+      httpsOrHttp({});
+    }).is.throwing('App is missing.');
+    done();
+  });
+
+  test('throws an error if certificate directory is missing.', done => {
+    assert.that(() => {
+      httpsOrHttp({ app: 'not-missing' });
+    }).is.throwing('Certificate directory is missing.');
+    done();
+  });
+
+  test('throws an error if ports are missing.', done => {
+    assert.that(() => {
+      httpsOrHttp({ certificateDirectory: 'not-missing', app: 'not-missing' });
+    }).is.throwing('Ports are missing.');
+    done();
+  });
+
+  test('throws an error if http port is missing.', done => {
+    assert.that(() => {
+      httpsOrHttp({ certificateDirectory: 'not-missing', app: 'not-missing', ports: {}});
+    }).is.throwing('Http port is missing.');
+    done();
+  });
+
+  test('throws an error if https port is missing.', done => {
+    assert.that(() => {
+      httpsOrHttp({ certificateDirectory: 'not-missing', app: 'not-missing', ports: { http: 'not-missing' }});
+    }).is.throwing('Https port is missing.');
+    done();
+  });
+
+  test('throws an error if callback is missing.', done => {
+    assert.that(() => {
+      httpsOrHttp({ certificateDirectory: 'not-missing', app: 'not-missing', ports: { http: 'not-missing', https: 'not-missing' }});
+    }).is.throwing('Callback is missing.');
+    done();
+  });
+
+  test('check if callback for HTTPS server called.', done => {
+    assert.that(() => {
+      const app = express();
+
+      httpsOrHttp({ app, certificateDirectory: path.join(__dirname, '..', 'certificates', 'localhost'), ports: { http: portHttp, https: portHttps }}, (err, result) => {
+        assert.that(err).is.null();
+        assert.that(result).is.equalTo({ app: { protocol: 'https', port: portHttps }, redirect: { protocol: 'http', port: portHttp }});
+        done();
+      });
+    }).is.not.throwing();
+  });
+
+  test('check if callback for HTTP server called.', done => {
+    assert.that(() => {
+      const app = express();
+
+      httpsOrHttp({ app, certificateDirectory: path.join(__dirname, '..', 'certificates', 'empty'), ports: { http: portHttp, https: portHttps }}, (err, result) => {
+        assert.that(err).is.null();
+        assert.that(result).is.equalTo({ app: { protocol: 'http', port: portHttp }});
+        done();
+      });
+    }).is.not.throwing();
+  });
+});
